6c899574a07aa32151157f9bf6826f0593dbbdc6
Gabriel Palacios <g.palaciossuarez@uandresbello.edu>
2024-08-24 01:57:15 +0000

    listo

---
+++ gato_11.py
@@ -0,0 +1,14 @@
+def main():
+    while True:
+        n = int(input("Ingresa un valor n mayor que 0: "))
+        if n < 0:
+            continue
+        else:
+            break
+    miau(n)
+
+def miau(n):
+    for i in range(n):
+        print("miau")
+
+main()
---
+++ gato_9.py
@@ -0,0 +1,6 @@
+while True:
+    n = int(input("Ingresa un valor n mayor que 0: "))
+    if n < 0:
+        continue
+    else:
+        break
