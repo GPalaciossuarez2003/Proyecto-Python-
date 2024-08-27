adb0badc35d5b67cb541767211a1c544adc22f60
Gabriel Palacios <g.palaciossuarez@uandresbello.edu>
2024-08-27 01:23:42 +0000

    listo

---
+++ precios.py
@@ -0,0 +1,16 @@
+precios = {
+    "Café": 1500,
+    "Té": 1000,
+    "Jugo Natural": 2000,
+    "Pastel de Chocolate": 2500,
+    "Tarta de Frutas": 3000,
+}
+
+pedidos = {
+    "Café": 1,
+    "Té": 0,
+    "Jugo Natural": 1,
+    "Pastel de Chocolate": 1,
+    "Tarta de Frutas": 1,
+}
+
