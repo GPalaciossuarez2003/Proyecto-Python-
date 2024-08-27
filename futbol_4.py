e7664938ec386204696e94ac86400f8dd3e0acab
Gabriel Palacios <g.palaciossuarez@uandresbello.edu>
2024-08-27 00:22:42 +0000

    listo

---
+++ futbol_4.py
@@ -0,0 +1,11 @@
+jugadores = {
+    "Vidal": "Colo Colo",
+    "Palacios": "Colo Colo",
+    "Falcon": "Colo Colo",
+    "Charles Aránguiz": "Universidad de Chile",
+    }
+
+print(jugadores["Vidal"])
+print(jugadores["Palacios"])
+print(jugadores["Falcon"])
+print(jugadores["Charles Aránguiz"])
---
+++ futbol_6.py
@@ -0,0 +1,9 @@
+jugadores = {
+    "Vidal": "Colo Colo",
+    "Palacios": "Colo Colo",
+    "Falcon": "Colo Colo",
+    "Charles Aránguiz": "Universidad de Chile",
+    }
+
+for jugador in jugadores:
+    print(jugador,jugadores[jugador])
